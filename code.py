d717f59aea8bdc6cb37d7e2549642598e5c4f7d1
djbios <dorandval@gmail.com>
2024-07-22 21:31:08 +0200

    basic asyncio

--- code.py
+++ code.py
@@ -3,9 +3,10 @@ import board
 import neopixel
 import time
 import board
-from utils import pwm_fade_in_out, Joint
+from utils import *
 import pwmio
 from adafruit_motor import servo
+import asyncio
 
 # Define the number of NeoPixels
 num_pixels = 1
@@ -14,49 +15,48 @@ pixel = neopixel.NeoPixel(board.NEOPIXEL, num_pixels)
 # Set the brightness
 pixel.brightness = 0.3
 
-# Define the rainbow colors
-rainbow_colors = [
-    (255, 0, 0),    # Red
-    (255, 127, 0),  # Orange
-    (255, 255, 0),  # Yellow
-    (0, 255, 0),    # Green
-    (0, 0, 255),    # Blue
-    (75, 0, 130),   # Indigo    (148, 0, 211)   # Violet
-]
 
-# Function to interpolate between two colors
-def interpolate_color(color1, color2, factor):
-    return (
-        int(color1[0] + (color2[0] - color1[0]) * factor),
-        int(color1[1] + (color2[1] - color1[1]) * factor),
-        int(color1[2] + (color2[2] - color1[2]) * factor)
-    )
-led_strip_pin = pwmio.PWMOut(board.GP8, frequency=100)
+
+def light_test():
+    print("Light test")
+    led_strip_pin = pwmio.PWMOut(board.GP8, frequency=100)
+
+    for _ in range(3):
+        pwm_fade_in_out(led_strip_pin, fade_time=0.5)
+    led_strip_pin.duty_cycle = 0
+    print("Light test done")
 
 
 
-#Perform 3 fade in and fade out cycles
-for _ in range(3):
-    pwm_fade_in_out(led_strip_pin, fade_time=0.2)
-led_strip_pin.duty_cycle = 0
+def leg_test():
+    print("Leg test")
+    leg1_joint1 = Joint(board.GP15)
+    leg1_joint1.move(30)
+    time.sleep(0.5)
+    leg1_joint1.move(180)
+    time.sleep(0.5)
+    leg1_joint1.move(90)
+    print("Leg test done")
 
-# Legs test
-leg1_joint1 = Joint(board.GP15)
+def rainbow():
+    print("Rainbow")
+    while True:
+        
+        # Main loop to cycle through the rainbow colors with smooth transitions
+        for i in range(len(rainbow_colors)):
+            start_color = rainbow_colors[i]
+            end_color = rainbow_colors[(i + 1) % len(rainbow_colors)]
+            for j in range(100):  # 100 steps for smooth transition
+                factor = j / 100.0
+                color = interpolate_color(start_color, end_color, factor)
+                pixel.fill(color)
+                time.sleep(0.01)  # Adjust speed of the gradient
 
-leg1_joint1.move(30)
-time.sleep(0.5)
-leg1_joint1.move(180)
-time.sleep(0.5)
-leg1_joint1.move(90)
+async def main():
+    print("Starting tests")
+    leg_test()
+    light_test()
 
-# Cycle
-while True:
-    # Main loop to cycle through the rainbow colors with smooth transitions
-    for i in range(len(rainbow_colors)):
-        start_color = rainbow_colors[i]
-        end_color = rainbow_colors[(i + 1) % len(rainbow_colors)]
-        for j in range(100):  # 100 steps for smooth transition
-            factor = j / 100.0
-            color = interpolate_color(start_color, end_color, factor)
-            pixel.fill(color)
-            time.sleep(0.01)  # Adjust speed of the gradient
+    rainbow()
+
+asyncio.run(main())
--- utils.py
+++ utils.py
@@ -14,6 +14,25 @@ def pwm_fade_in_out(pwm_pin, fade_time=0.5, steps=100):
         time.sleep(step_delay)
 
 
+# Define the rainbow colors
+rainbow_colors = [
+    (255, 0, 0),    # Red
+    (255, 127, 0),  # Orange
+    (255, 255, 0),  # Yellow
+    (0, 255, 0),    # Green
+    (0, 0, 255),    # Blue
+    (75, 0, 130),   # Indigo    (148, 0, 211)   # Violet
+]
+
+# Function to interpolate between two colors
+def interpolate_color(color1, color2, factor):
+    return (
+        int(color1[0] + (color2[0] - color1[0]) * factor),
+        int(color1[1] + (color2[1] - color1[1]) * factor),
+        int(color1[2] + (color2[2] - color1[2]) * factor)
+    )
+
+
 class Joint:
     def __init__(self, pin):
         self.pwm = PWMOut(pin, duty_cycle=2 ** 15, frequency=50)

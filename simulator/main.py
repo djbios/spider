c2369db13f79ce1f5754931f7f4d021d62cdce7f
djbios <dorandval@gmail.com>
2024-08-08 12:30:39 +0200

    refac

--- simulator/main.py
+++ simulator/main.py
@@ -26,46 +26,42 @@ DEBOUNCE_DELAY = 300  # ms
 FIGURE_SIZE = (10, 10)
 AX_LIMIT = [-50, 50]
 
-class Leg:
-    def __init__(self):
-        self.chain = Chain(name='leg1', links=[
-            OriginLink(),
-            URDFLink(
-                name="hip",
-                bounds=(math.radians(0), math.radians(180)),
-                origin_translation=[10, -10, 0],
-                origin_orientation=[0, 0, math.radians(-90)],
-                rotation=[0, 0, 1],
-            ),
-            URDFLink(
-                name="knee",
-                bounds=(math.radians(0), math.radians(180)),
-                origin_translation=[10, 0, 0],
-                origin_orientation=[0, math.radians(180), 0],
-                rotation=[0, -1, 0],
-            ),
-            URDFLink(
-                name="ankle",
-                bounds=(math.radians(0), math.radians(180)),
-                origin_translation=[0, 0, 10],
-                origin_orientation=[0, math.radians(-90), 0],
-                rotation=[0, 1, 0],
-            ),
-            URDFLink(
-                name="pyatka",
-                origin_translation=[10, 0, 0],
-                origin_orientation=[0, 0, 0],
-                rotation=[1, 0, 0],
-            )
-        ])
+leg1 = Chain(name='leg1', links=[
+    OriginLink(),
+    URDFLink(
+        name="hip",
+        bounds=(math.radians(0), math.radians(180)),
+        origin_translation=[10, -10, 0],
+        origin_orientation=[0, 0, math.radians(-90)],
+        rotation=[0, 0, 1],
+    ),
+    URDFLink(
+        name="knee",
+        bounds=(math.radians(0), math.radians(180)),
+        origin_translation=[10, 0, 0],
+        origin_orientation=[0, math.radians(180), 0],
+        rotation=[0, -1, 0],
+    ),
+    URDFLink(
+        name="ankle",
+        bounds=(math.radians(0), math.radians(180)),
+        origin_translation=[0, 0, 10],
+        origin_orientation=[0, math.radians(-90), 0],
+        rotation=[0, 1, 0],
+    ),
+    URDFLink(
+        name="pyatka",
+        origin_translation=[10, 0, 0],
+        origin_orientation=[0, 0, 0],
+        rotation=[1, 0, 0],
+    )
+])
 
-    def calculate_ik(self, position):
-        return self.chain.inverse_kinematics(position)
 
 class IKLegGUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.leg = Leg()
+        self.leg1 = leg1
         self.initUI()
 
     def initUI(self):
@@ -74,8 +70,7 @@ class IKLegGUI(QWidget):
         self.canvas = FigureCanvasQTAgg(self.figure)
         self.ax = self.figure.add_subplot(111, projection='3d')
 
-        # Initial plot
-        self.update_plot([INITIAL_X, INITIAL_Y, INITIAL_Z])
+
 
         # Create sliders for coordinates
         self.x_slider, self.x_label = self.create_slider_with_label(X_MIN, X_MAX, INITIAL_X, 'X', 0.1)
@@ -83,9 +78,12 @@ class IKLegGUI(QWidget):
         self.z_slider, self.z_label = self.create_slider_with_label(Z_MIN, Z_MAX, INITIAL_Z, 'Z', 0.1)
 
         # Create sliders for angles
-        self.hip_angle_slider, self.hip_angle_label = self.create_slider_with_label(ANGLE_MIN, ANGLE_MAX, INITIAL_HIP_ANGLE, 'Hip', 1)
-        self.knee_angle_slider, self.knee_angle_label = self.create_slider_with_label(ANGLE_MIN, ANGLE_MAX, INITIAL_KNEE_ANGLE, 'Knee', 1)
-        self.ankle_angle_slider, self.ankle_angle_label = self.create_slider_with_label(ANGLE_MIN, ANGLE_MAX, INITIAL_ANKLE_ANGLE, 'Ankle', 1)
+        self.hip_angle_slider, self.hip_angle_label = self.create_slider_with_label(ANGLE_MIN, ANGLE_MAX,
+                                                                                    INITIAL_HIP_ANGLE, 'Hip', 1)
+        self.knee_angle_slider, self.knee_angle_label = self.create_slider_with_label(ANGLE_MIN, ANGLE_MAX,
+                                                                                      INITIAL_KNEE_ANGLE, 'Knee', 1)
+        self.ankle_angle_slider, self.ankle_angle_label = self.create_slider_with_label(ANGLE_MIN, ANGLE_MAX,
+                                                                                        INITIAL_ANKLE_ANGLE, 'Ankle', 1)
 
         # Layout
         layout = QGridLayout()
@@ -104,6 +102,8 @@ class IKLegGUI(QWidget):
         self.timer = QTimer(self)
         self.timer.setSingleShot(True)
         self.timer.timeout.connect(self.update_leg)
+
+        self.update_plot([INITIAL_X, INITIAL_Y, INITIAL_Z])
 
     def create_slider_with_label(self, min_val, max_val, initial_val, label_text, step):
         slider = QSlider(Qt.Horizontal)
@@ -135,23 +135,20 @@ class IKLegGUI(QWidget):
         y = self.y_slider.value()
         z = self.z_slider.value()
         self.update_plot([x, y, z])
-        self.update_angle_sliders()
 
     def update_plot(self, position):
-        ik = self.leg.calculate_ik(position)
+        ik = self.leg1.inverse_kinematics(position)
         self.ax.cla()
-        self.leg.chain.plot(ik, self.ax, show=False)
+        self.leg1.plot(ik, self.ax, show=False)
         self.ax.scatter([position[0]], [position[1]], [position[2]], color='r', s=100)
         self.ax.set_xlim(AX_LIMIT)
         self.ax.set_ylim(AX_LIMIT)
         self.ax.set_zlim(AX_LIMIT)
         self.canvas.draw()
-
-    def update_angle_sliders(self):
-        ik = self.leg.calculate_ik([self.x_slider.value(), self.y_slider.value(), self.z_slider.value()])
         self.hip_angle_slider.setValue(int(math.degrees(ik[1])))
         self.knee_angle_slider.setValue(int(math.degrees(ik[2])))
         self.ankle_angle_slider.setValue(int(math.degrees(ik[3])))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
